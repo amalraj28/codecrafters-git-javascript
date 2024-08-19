a7e8d87d94bdd651e4d6355e8d3ac652b3413349
Amal Raj <amalraj2701@gmail.com>
2024-08-19 16:26:29 +0530

    Stage-1 done

---
+++ app/git/commands/cat-file.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const zlib = require("zlib");
+const fs = require("fs");
+
+class CatFileCommand {
+	constructor(flag, hash) {
+		this.flag = flag;
+		this.hash = hash;
+	}
+
+	execute() {
+		const flag = this.flag;
+		const hash = this.hash;
+
+		switch (flag) {
+			case "-p":
+				{
+					const folder = hash.slice(0, 2);
+					const file = hash.slice(2);
+
+					const fullPath = path.join(
+						process.cwd(),
+						".git",
+						"objects",
+						folder,
+						file
+					);
+
+					if (!fs.existsSync(fullPath))
+						throw new Error(`Not a valid object name ${hash}`);
+
+					const fileContents = fs.readFileSync(fullPath);
+					const outputBuffer = zlib.inflateSync(fileContents);
+					const output = outputBuffer.toString().trim();
+
+					process.stdout.write(output);
+				}
+				break;
+		}
+	}
+}
+
+module.exports = CatFileCommand;
